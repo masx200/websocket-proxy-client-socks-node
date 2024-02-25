996a1e6a12838d54d37254948cfc157fef53bd53
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-25 17:22:26 +0800

    支持基本身份验证,用户名和密码

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 
 websocket-proxy-client-socks-node
 
+支持基本身份验证,用户名和密码
+
 #### 软件架构
 
 软件架构说明
 
-websocket-proxy-deno-deploy的socks客户端
+websocket-proxy-deno-deploy 的 socks 客户端
 
 运行
 
@@ -24,6 +26,10 @@ node ./cli.js
   "server_url": "ws://localhost:8000",
   //本地socks服务器的端口
   "sport": 1080,
+  //远程服务器的用户名,可以为null
+  "server_username": "hello",
+  //远程服务器的用户名,可以为null
+  "server_password": "world",
   //本地socks服务器的地址
   "sbind": "0.0.0.0"
 }
--- cli.js
+++ cli.js
@@ -7,7 +7,8 @@ const { WebSocket, createWebSocketStream } = require("ws");
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 
-const socks = async ({ server_url, sport = 1080, sbind = "127.0.0.1" }) => {
+async function socks(config) {
+  const { server_url, sport = 1080, sbind = "127.0.0.1" } = config;
   const url = server_url;
   net
     .createServer((socks) =>
@@ -21,34 +22,51 @@ const socks = async ({ server_url, sport = 1080, sbind = "127.0.0.1" }) => {
               const [VERSION, CMD, RSV, ATYP] = head;
               if (RSV != 0x00) return socks.end();
               if (VERSION != 0x05 || CMD != 0x01) return;
-              const host = ATYP == 0x01
-                ? head
-                  .slice(4, -2)
-                  .map((b) => parseInt(b, 10))
-                  .join(".")
-                : ATYP == 0x04
-                ? head
-                  .slice(4, -2)
-                  .reduce(
-                    (s, b, i, a) => i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s,
-                    [],
-                  )
-                  .map((b) => b.readUInt16BE(0).toString(16))
-                  .join(":")
-                : ATYP == 0x03
-                ? head.slice(5, -2).toString("utf8")
-                : "";
+              const host =
+                ATYP == 0x01
+                  ? head
+                      .slice(4, -2)
+                      .map((b) => parseInt(b, 10))
+                      .join(".")
+                  : ATYP == 0x04
+                  ? head
+                      .slice(4, -2)
+                      .reduce(
+                        (s, b, i, a) =>
+                          i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s,
+                        []
+                      )
+                      .map((b) => b.readUInt16BE(0).toString(16))
+                      .join(":")
+                  : ATYP == 0x03
+                  ? head.slice(5, -2).toString("utf8")
+                  : "";
               const port = head.slice(-2).readUInt16BE(0);
 
               new Promise((res, rej) => {
                 {
-                  const opts = {
-                    headers: {
-                      "X-Destination-Address": host,
-                      "X-Destination-Port": port,
-                      "x-Protocol": "CONNECT",
-                    },
+                  const headers = {
+                    "X-Destination-Address": host,
+                    "X-Destination-Port": port,
+                    "x-Protocol": "CONNECT",
                   };
+                  const opts = {
+                    headers: headers,
+                  };
+                  /* 
+支持基本身份验证,用户名和密码 */
+                  if (
+                    config["server_username"] != null &&
+                    config["server_password"]
+                  ) {
+                    headers["Authorization"] =
+                      "Basic " +
+                      btoa(
+                        config["server_username"] +
+                          ":" +
+                          config["server_password"]
+                      );
+                  }
                   console.log(opts);
                   new WebSocket(url, opts)
                     .on("open", function (e) {
@@ -78,7 +96,7 @@ const socks = async ({ server_url, sport = 1080, sbind = "127.0.0.1" }) => {
     )
     .listen(sport, sbind, logcb(`server start on: ${sbind}:${sport}`))
     .on("error", (e) => errcb("socks5-err")(e.message));
-};
+}
 fs.exists(cpath, (e) => {
   if (e) socks(JSON.parse(fs.readFileSync(cpath)));
   else console.error("当前程序的目录没有config.json文件!");
