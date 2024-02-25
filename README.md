226c1103381d8dd737ca434db9b733cd47e89552
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-25 14:31:30 +0800

    /* https://github.com/imTheSabrali/CF-Worker-Socks */

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # websocket-proxy-client-socks-node
 
 #### 介绍
+
 websocket-proxy-client-socks-node
 
 #### 软件架构
+
 软件架构说明
 
 websocket-proxy-deno-deploy的socks客户端
 
-
 运行
+
 ```
 node ./cli.js
 ```
@@ -18,11 +20,11 @@ node ./cli.js
 
 ```json
 {
-//服务端websocket的url链接
-    "server_url": "ws://localhost:8000",
-//本地socks服务器的端口
-    "sport": 1080,
-//本地socks服务器的地址
-    "sbind": "0.0.0.0"
+  //服务端websocket的url链接
+  "server_url": "ws://localhost:8000",
+  //本地socks服务器的端口
+  "sport": 1080,
+  //本地socks服务器的地址
+  "sbind": "0.0.0.0"
 }
 ```
--- cli.js
+++ cli.js
@@ -1,75 +1,47 @@
 #!/usr/bin/env node
-//@DNetL
+/* https://github.com/imTheSabrali/CF-Worker-Socks */
 const cpath = "./config.json";
 const fs = require("fs");
 const net = require("net");
-// const https = require("https");
 const { WebSocket, createWebSocketStream } = require("ws");
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
-//,
-//   cache = {};
 
-const socks = async ({
-  server_url,
-  //   psw,
-  sport = 1080,
-  sbind = "127.0.0.1",
-  //   wkip,
-  //   byip,
-  //   cfhs = [],
-}) => {
-  // Object.assign(CFDOMAN, cfhs);
-  // if (wkip) {
-  //   Object.assign(opts, {
-  //     lookup: (host, opts, cb) =>
-  //       cb(null, wkip, wkip.indexOf(":") == -1 ? 4 : 6),
-  //   });
-  // }
+const socks = async ({ server_url, sport = 1080, sbind = "127.0.0.1" }) => {
   const url = server_url;
   net
     .createServer((socks) =>
       socks
         .once("data", (data) => {
-          const [VERSION] = data; //VERSION NMETHODS METHODS
+          const [VERSION] = data;
           if (VERSION != 0x05) socks.end();
           else if (data.slice(2).some((method) => method == 0x00)) {
-            //0x00,0x02
-            socks.write(Buffer.from([0x05, 0x00])); //select
+            socks.write(Buffer.from([0x05, 0x00]));
             socks.once("data", (head) => {
               const [VERSION, CMD, RSV, ATYP] = head;
               if (RSV != 0x00) return socks.end();
-              if (VERSION != 0x05 || CMD != 0x01) return; //connect
-              const host =
-                ATYP == 0x01
-                  ? head
-                      .slice(4, -2)
-                      .map((b) => parseInt(b, 10))
-                      .join(".") //IPV4
-                  : ATYP == 0x04
-                  ? head
-                      .slice(4, -2)
-                      .reduce(
-                        (s, b, i, a) =>
-                          i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s,
-                        []
-                      )
-                      .map((b) => b.readUInt16BE(0).toString(16))
-                      .join(":") //IPV6
-                  : ATYP == 0x03
-                  ? head.slice(5, -2).toString("utf8")
-                  : ""; //DOMAIN
+              if (VERSION != 0x05 || CMD != 0x01) return;
+              const host = ATYP == 0x01
+                ? head
+                  .slice(4, -2)
+                  .map((b) => parseInt(b, 10))
+                  .join(".")
+                : ATYP == 0x04
+                ? head
+                  .slice(4, -2)
+                  .reduce(
+                    (s, b, i, a) => i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s,
+                    [],
+                  )
+                  .map((b) => b.readUInt16BE(0).toString(16))
+                  .join(":")
+                : ATYP == 0x03
+                ? head.slice(5, -2).toString("utf8")
+                : "";
               const port = head.slice(-2).readUInt16BE(0);
 
-              // isCFIP(host, ATYP).then(({ cf, ip }) => {
               new Promise((res, rej) => {
-                /*   if (cf && !byip) {
-                  net
-                    .connect(port, wkip ? wkip : ip, function () {
-                      res(this);
-                    })
-                    .on("error", rej);
-                } else */ {
+                {
                   const opts = {
                     headers: {
                       "X-Destination-Address": host,
@@ -80,13 +52,6 @@ const socks = async ({
                   console.log(opts);
                   new WebSocket(url, opts)
                     .on("open", function (e) {
-                      // this.send(
-                      //   JSON.stringify({
-                      //     hostname: cf ? byip : ip,
-                      //     port,
-                      //     // psw,
-                      //   })
-                      // );
                       res(createWebSocketStream(this));
                     })
                     .on("error", (e) => rej(e));
@@ -94,7 +59,7 @@ const socks = async ({
               })
                 .then((s) => {
                   socks.write(((head[1] = 0x00), head));
-                  logcb("conn:")({ host, port /* cf */ });
+                  logcb("conn:")({ host, port });
                   socks
                     .pipe(s)
                     .on("error", (e) => errcb("E1:")(e.message))
@@ -107,8 +72,7 @@ const socks = async ({
                   socks.end(((head[1] = 0x03), head));
                 });
             });
-            // });
-          } else socks.write(Buffer.from([0x05, 0xff])); //reject
+          } else socks.write(Buffer.from([0x05, 0xff]));
         })
         .on("error", (e) => errcb("socks-err:")(e.message))
     )
